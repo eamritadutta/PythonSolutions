0bfe3a2d77487858f8800eebcdfd6727c9f163d8
Amrita Dutta <amrita@blabpredicts.com>
2015-09-14 09:37:41 -0700

    Implemented recursive tree node deleting. Root deleting has not been handled yet.

--- DeleteNodeInTree.py
+++ DeleteNodeInTree.py
@@ -64,6 +64,55 @@ class Tree:
 
         self.add(self.root, val)
 
+    # the following method will find the root with val and then delete it from
+    # the tree and adjust the tree accordingly. for adjusting the tree accordingly, this function needs to keep track of the parent nodes as it
+    # traverses the tree
+    def deleteNode(self, val):
+
+        # if the root is None, always do this sanity test
+        if self.root == None:
+            print "Empty tree passed-in to the deleteNode method. Returning."
+            return
+
+        # recursivly delete
+        # pass - in the node where we start looking for val
+        # also pass - in the value of the parent with each recursive call
+        if self.deleteNodeRecursively(self.root, None, val) == False:
+            print "Did not find node wiht value: " + str(val) + " in the given Tree. No node has been deleted."
+
+        else:
+            print "Deleted node with value: " + str(val) + " from the given Tree."
+
+    def deleteNodeRecursively(self, start, parent, val):
+
+        # base case - check if start is None, return false
+        if start == None:
+            return False
+
+        # found the node to be deleted on the right of the parent
+        if val == start.val and val > parent.val:
+            # make the left child, if any of this node the right child of the parent. if not, make the right child of this node the right child of the parent
+            if start.left != None:
+                parent.right = start.left
+            else:
+                parent.right = start.right
+
+        # found the node with 'val' to be deleted on the left of the parent node
+        elif val == start.val and val <= parent.val:
+
+            # check for left child of node with 'val'
+            if start.left != None:
+                parent.left = start.left
+            else:
+                parent.left = start.left
+
+        # we did not find the node with 'val' to be deleted
+        # have to check both the left and the right child of 'start'
+        elif self.deleteNodeRecursively(start.left, start, val) == False:
+                return self.deleteNodeRecursively(start.right, start, val)
+        else:
+            return False
+
     def printTree(self):
 
         # prints a tree recursively using depth first traversal
@@ -89,8 +138,18 @@ t.addNode(100)
 t.addNode(1)
 t.addNode(200)
 
-#print t.root.left.val
-#print t.root.right.val
+print "------- Printing Original Tree --------"
+t.printTree()
 
-print "------- Printing Tree --------"
+t.deleteNode(100)
+print "------- Printing Tree after deleting node --------"
+t.printTree()
+
+
+t.deleteNode(10)
+print "------- Printing Tree after deleting node --------"
+t.printTree()
+
+t.deleteNode(1)
+print "------- Printing Tree after deleting node --------"
 t.printTree()
